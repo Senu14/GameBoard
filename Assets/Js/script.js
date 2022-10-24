05fcc745ec1aa06dfafff2b8700c09353083ab06
Senu14 <101559338+Senu14@users.noreply.github.com>
2022-10-24 12:45:35 +0200

    Changes1

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,16 +1,17 @@
 //objects
-import {GoalModel} from "/model.js"
-const  gameboard=document.getElementById("gameboard") 
+import { GoalModel } from "/model.js"
+const  gameboard=document.getElementById('gameboard') 
 const numCards= 5;
 const cardList= GoalModel()
-cardList.sort(() => Match.random()-0.5);
-cardList.cardList.slice(0, numCards)
-cardList.cardList.concat(cardList)
-cardList.sort(() => Match.random()-0.5);
+cardList.sort(() => Math.random()-0.5);
+cardList=cardList.slice(0,numCards)
+cardList=cardList.concat(cardList)
+cardList.sort(() => Math.random()-0.5);
 
 
 for(let card of cardList){
-       let div = document.createElement("div")
-       div.innerText = cardgoal
+       let div = document.createElement('div')
+       div.innerText = card.goal
        gameboard.append(div)
 }
+console.log(GoalModel())
