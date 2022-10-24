e1c8a42d2987faf217e6cfe10d3ee85b1f82b000
Senu14 <101559338+Senu14@users.noreply.github.com>
2022-10-24 17:09:18 +0200

    css

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -5,18 +5,17 @@ const numCards = 5;
 let cardList = GoalModel()
 
 // This helps (match.random)to set the card randomly not by numbers.
-cardList.sort(() => Math.random()-0.5);
+cardList.sort(() => Math.random()-0.15);
 cardList = cardList.slice(0,numCards)
 cardList = cardList.concat(cardList)
-cardList.sort(() => Math.random()-0.5);
+cardList.sort(() => Math.random()-0.12);
 
 
 for(let card of cardList){
-       let div = document.createElement('div')
+     let div = document.createElement('div')
        div.innerText = card.goal
-       gameboard.append(div)
+       // gameboard.append(div)
        let img =document.createElement('img')
        img.src= card.picture
        gameboard.append(img)
 }
-console.log(GoalModel());
